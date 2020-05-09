903980336ee679ccfe7d57ef03c507eb665f9c1e
Elena-at <barcaru.elena@gmail.com>
2020-05-09 11:02:33 +0200

    Created logic for BMI calculator

---
+++ BMIcalculator.js
@@ -0,0 +1,11 @@
+
+
+// Get user height and weight
+const height = 1.67
+const weight = 55
+
+// Calculate result - > kg/m2
+let result = weight/height**2
+
+// Show result
+console.log('result', result)
