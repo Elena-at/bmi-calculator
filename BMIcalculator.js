4ddda61ad13162178ac6596c0480c508c8f5942b
Elena-at <barcaru.elena@gmail.com>
2020-05-14 12:12:40 +0200

    Integration with html

--- BMIcalculator.js
+++ BMIcalculator.js
@@ -1,25 +1,36 @@
-const height = 1.71
-// prompt("Insert your height (cm):");
-const weight = 60
-// prompt("Insert your weight (kg)");
 
-const BMI = weight/(height*height);
+ const calculateBMI = function()//no need for parameters it knows where to get the information from
+ {
+ const height= document.getElementById('cm').value;
+ console.log(height)
+ const weight = document.getElementById('kg').value;
+ console.log(weight)
+ return weight / height**2 
+ }
 
 
-let result = "Your BMI " + BMI;
+ function getBMIresult() {
+   const BMI =calculateBMI();
+   let result = 'Your BMI is';
 
-if (BMI < 18.5) {  
+
+  if (BMI < 18.5) {  
     result = `${result} ${BMI}, You are underweight`;
-} else if (BMI > 18.5 && BMI < 25){
-     result = `${result} ${BMI}, You have a normal weight`;
+ } else if (BMI > 18.5 && BMI < 25){
+    result = `${result} ${BMI}, You have a normal weight`;
 } else if (BMI > 25 && BMI < 30){
-    result = `${result} ${BMI}, You are overweight`;
-} else if ( BMI > 30) {
-    result = `${result} ${BMI}, You are obese`;
-}
+    result = `${result} ${BMI}'You are overweight`;
+ } else if ( BMI > 30) {
+    result = `${result} ${BMI}'You are obese`;
+ }
+ 
+ document.querySelector('.BMIcategories').textContent=results
+ // console.log(result) instead of above
 
+ // const result = calculateBMI(weight, height);
+// getBMIresult(result) same as bellow
 
-
+getBMIresults(calculateBMI(weight, height));
 
 
 
